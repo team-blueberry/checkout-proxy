377e754df59dec0e78557297504bf1dc4c0e83a5
Lexi Santoro <lexisantoro1@gmail.com>
2019-01-19 16:10:34 -0500

    proxy working on local

--- server/index.js
+++ server/index.js
@@ -3,37 +3,40 @@ const app = express();
 const bodyParser = require('body-parser');
 const request = require('request');
 const compression = require('compression');
+const axios = require('axios')
 
 app.use(compression());
-app.use(express.static(__dirname + '/../public/'));
+app.use('/:id', express.static(__dirname + '/../public/'));
 app.use(bodyParser.json());
 
-// app.use((req,res,next) => {
-//   console.log(req.url);
-//   let proxy = 'http://ec2-3-83-149-86.compute-1.amazonaws.com:3016' + req.url;
-//   console.log(proxy);
-//   request(proxy).pipe(res);
-//   next()
-// }, (req,res,next) => {
-//     console.log('ari, ',req.url);
-//     let proxy = 'http://ec2-13-59-251-226.us-east-2.compute.amazonaws.com' + req.url;
-//     console.log(proxy);
-//     request(proxy).pipe(res);
-// });
-app.use((req, res, next) => {
- res.header("Access-Control-Allow-Origin", "*");
- res.header(
-   "Access-Control-Allow-Headers",
-   "Origin, X-Requested-With, Content-Type, Accept"
- );
- next();
-});
-// app.use((req,res, next) => {
-//   console.log(req.url);
-//   let proxy = 'http://ec2-3-83-149-86.compute-1.amazonaws.com:3016' + req.url;
-//   console.log(proxy);
-//   request(proxy).pipe(res);
-// });
+//carousel
+const carousel = {host: 'http://34.233.106.94'
+  , endpoint : '/api/images/'} ;
+//checkout
+const checkout = {host: 'http://ec2-18-223-214-235.us-east-2.compute.amazonaws.com'
+  , endpoint : '/api/checkout/'} ;
+//reviews
+const reviews = {host: 'http://ec2-18-223-135-118.us-east-2.compute.amazonaws.com'
+  , endpoint : '/api/reviews/'} ;
+
+var appGet = function(instance) {
+  app.get(instance.endpoint + ':id', (req, res) => {
+    let id = req.params.id;
+    axios
+      .get(instance.host + instance.endpoint + id)
+      .then(result => {
+        res.send(result.data);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  });
+}
+
+appGet(carousel);
+appGet(checkout);
+appGet(reviews);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
